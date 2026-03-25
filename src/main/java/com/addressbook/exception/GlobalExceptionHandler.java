d48b42119c9bf6f26cf4cb0e2e680b01f074af04
smanaspratap <manaspratap2003@gmail.com>
2026-03-25 16:24:16 +0530

    [Manas]: Section 4 UC2 - Implement GlobalExceptionHandler for MethodArgumentNotValidException

---
+++ src/main/java/com/addressbook/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+﻿package com.addressbook.exception;
+
+import com.addressbook.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Global Exception Handler annotated with @ControllerAdvice.
+ * Intercepts exceptions across the application and formats them nicely.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Handles validation failures (HTTP 400 Bad Request)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errMesg = errorList.stream()
+                .map(objErr -> objErr.getDefaultMessage())
+                .collect(Collectors.toList());
+        
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request", errMesg);
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+}
