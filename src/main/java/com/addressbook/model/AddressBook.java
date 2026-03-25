178c706fd02d6596a643948ad8e5e574930a52f6
smanaspratap <manaspratap2003@gmail.com>
2026-03-25 16:23:45 +0530

    [Manas]: Section 3 UC1 - Introduce DTO and Model using Lombok annotations

---
+++ src/main/java/com/addressbook/model/AddressBook.java
@@ -0,0 +1,20 @@
+﻿package com.addressbook.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+/**
+ * Domain model representing an Address Book contact.
+ * Utilizes Lombok for boilerplate code reduction.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressBook {
+    private long id;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+}
